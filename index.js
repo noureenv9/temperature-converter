6c01124df72432ef5e19e55e072ecf6be4153338
unknown <noureensoliman248@gmail.com>
2025-04-22 21:30:52 +0200

    Initial commit: Added dice roller project

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-function convert() {
-    let inputtemp = parseFloat(document.getElementById("textbox").value); 
-    let result = document.getElementById("result"); 
-    let selectunit ; 
-    let radios = document.querySelectorAll('input[type="radio"]');
-    for (let radio of radios){
-        if (radio.checked) {
-            selectunit = radio.id;
-            break;
-        }
+function rolldice(){
+    const numofdice = document.getElementById("numofdice").value;
+    const diceresult= document.getElementById("diceresult");
+    const diceimage = document.getElementById("diceimage");
+    const values = [];
+    const images = [];
+    for (let i = 0; i < numofdice; i++) {
+        const randomValue = Math.floor(Math.random() * 6) + 1;
+        values.push(randomValue);
+        images.push(`<img src="./diced_img/${randomValue}.png" alt="Dice ${randomValue}">`);
     }
-    if (!selectunit){
-        result.innerHTML= "please select a conversion type !";
-        return;
-    }
-    let outputtemp ;
-    if (selectunit==="tofahrenheit"){
-        outputtemp= (inputtemp * 9/5) +32 ;
-        result.innerHTML= `${inputtemp}°C = ${outputtemp.toFixed(2)}°F`;
-    } else if (selectunit==="tocelsius") {
-        outputtemp = (inputtemp- 32) * 5/9 ;
-        result.innerHTML= `${inputtemp}°F = ${outputtemp.toFixed(2)}°C`;
-    } else if (selectunit==="tokelvin"){
-        outputtemp= inputtemp - 273.15 ;
-        result.innerHTML= `${inputtemp}°K= ${outputtemp.toFixed(2)}°C`;
-    } else if (selectunit==="tokelvin2"){
-        outputtemp = (inputtemp * 9/5) - 459.67 ;
-        result.innerHTML= `${inputtemp}°K = ${outputtemp.toFixed(2)}°F`;
-    } else {
-        result.innerHTML = "Invalid Selection";
-    }
+    diceresult.textContent = `dice rolled : ${values.join(', ')}`;
+    diceimage.innerHTML=images.join('  '); 
 }
